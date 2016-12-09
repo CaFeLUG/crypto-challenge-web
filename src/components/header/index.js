172b3057c64f5bbe73e6a0f4ddc757ff770842fd
Federico Chpriani <sietedosfede@gmail.com>
2016-12-09 13:43:14 -0300

    Update index.js

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -12,21 +12,21 @@ export default class Header extends Component {
 		let path = (location.pathname).substr(1);
 		switch(path){
 			case '': this.setState({ a: 'Desafios', ahref: '/Desafios',
-															 b: 'Ranking', bhref: '/Ranking',
-															 c: 'Reglas', chref: '/Reglas' });
-			break;
+				 b: 'Ranking', bhref: '/Ranking',
+				 c: 'Reglas', chref: '/Reglas' });
+				 break;
 			case 'Desafios': this.setState({ a: 'Perfil', ahref: '/',
-			 																 b: 'Ranking', bhref: '/Ranking',
-																			 c: 'Reglas', chref: '/Reglas'});
-			break;
+			 		 b: 'Ranking', bhref: '/Ranking',
+					 c: 'Reglas', chref: '/Reglas'});
+					 break;
 			case 'Ranking': this.setState({ a: 'Perfil', ahref: '/',
-			 															  b: 'Desafios', bhref: '/Desafios',
-																		  c: 'Reglas', chref: '/Reglas'});
-			break;
+			 		b: 'Desafios', bhref: '/Desafios',
+					c: 'Reglas', chref: '/Reglas'});
+					break;
 			case 'Reglas': this.setState({ a: 'Perfil', ahref: '/',
-			 															 b: 'Desafios', bhref: '/Desafios',
-																		 c: 'Ranking', chref: '/Ranking'});
-			break;
+				       b: 'Desafios', bhref: '/Desafios',
+				       c: 'Ranking', chref: '/Ranking'});
+				       break;
 		}
 	}
 	render(props,{a,ahref,b,bhref,c,chref}){
